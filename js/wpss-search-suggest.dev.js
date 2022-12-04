dac8ec6e1749378bab42000c7a67d0568a948bb3
Konstantin Obenland <obenland@gmx.de>
2022-12-03 22:25:20 -0700

    Fix phpcs and simplify (#6)

--- js/wpss-search-suggest.dev.js
+++ js/wpss-search-suggest.dev.js
@@ -1,21 +1,32 @@
-jQuery( function( $ ) {
-	$( '#s,[name="s"]' ).suggest( wpss_options.ajaxurl, {
-		onSelect: function() {
-			var $form = $( this ).parents( 'form' );
+jQuery(
+	function( $ ) {
+		$( '#s,[name="s"]' ).suggest(
+			wpss_options.ajaxurl,
+			{
+				onSelect: function() {
+					 var $form = $( this ).parents( 'form' );
 
-			$.post( wpss_options.url, {
-				action: 'wpss-post-url',
-				_wpnonce: wpss_options.nonce,
-				title: $( '.ac_over' ).text()
-			}, function( data ) {
-				if ( data ) {
-					window.location = data;
-				} else {
-					$form.submit();
+					$.post(
+						wpss_options.url,
+						{
+							action: 'wpss-post-url',
+							_wpnonce: wpss_options.nonce,
+							title: $( '.ac_over' ).text()
+						 },
+						function( data ) {
+							if ( data ) {
+								 window.location = data;
+							} else {
+									 $form.submit();
+							}
+						}
+					).fail(
+						function() {
+							$form.submit();
+						}
+					);
 				}
-			}).fail( function() {
-				$form.submit();
-			});
-		}
-	});
-});
+			}
+		);
+	}
+);
