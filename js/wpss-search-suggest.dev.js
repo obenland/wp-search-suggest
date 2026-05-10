a6478e820e56ed64305dcdcace83b59b420b9f11
Konstantin Obenland <obenland@gmx.de>
2026-05-09 22:06:55 -0500

    Format search suggest JS and update env
    Reformat js/wpss-search-suggest.dev.js for consistent spacing and indentation (normalized jQuery ready wrapper, reorganized $.post arguments, moved .fail handling, added trailing comma after title). Make a small .wp-env.json formatting change to compact the plugins array. package-lock.json was regenerated/updated (lockfile changes only). No functional behaviour intended to change.

--- js/wpss-search-suggest.dev.js
+++ js/wpss-search-suggest.dev.js
@@ -1,32 +1,25 @@
-jQuery(
-	function( $ ) {
-		$( '#s,[name="s"]' ).suggest(
-			wpss_options.ajaxurl,
-			{
-				onSelect: function() {
-					 var $form = $( this ).parents( 'form' );
+jQuery( function ( $ ) {
+	$( '#s,[name="s"]' ).suggest( wpss_options.ajaxurl, {
+		onSelect: function () {
+			var $form = $( this ).parents( 'form' );
 
-					$.post(
-						wpss_options.url,
-						{
-							action: 'wpss-post-url',
-							_wpnonce: wpss_options.nonce,
-							title: $( '.ac_over' ).text()
-						 },
-						function( data ) {
-							if ( data ) {
-								 window.location = data;
-							} else {
-									 $form.submit();
-							}
-						}
-					).fail(
-						function() {
-							$form.submit();
-						}
-					);
+			$.post(
+				wpss_options.url,
+				{
+					action: 'wpss-post-url',
+					_wpnonce: wpss_options.nonce,
+					title: $( '.ac_over' ).text(),
+				},
+				function ( data ) {
+					if ( data ) {
+						window.location = data;
+					} else {
+						$form.submit();
+					}
 				}
-			}
-		);
-	}
-);
+			).fail( function () {
+				$form.submit();
+			} );
+		},
+	} );
+} );
